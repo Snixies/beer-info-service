01bfbabacf054a31741a27cba4e68bef438dcb0b
Snixies <79220714+Snixies@users.noreply.github.com>
2021-05-19 11:41:43 +0200

    startup spring boot application

---
+++ src/main/java/Snixies/beerinfoservice/BeerInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package Snixies.beerinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerInfoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Snixies/beerinfoservice/BeerInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Snixies.beerinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
